f227f865e9dc3ec7f5367dc9feb2d4327b0ad814
7vTr0x7 <7vtrox7@gmail.com>
2026-07-23 17:37:11 +0530

    call

--- rev/funda/call.js
+++ rev/funda/call.js
@@ -1,1 +1,16 @@
+const obj = {
+  name: "non",
+};
 
+function normal(city, state) {
+  console.log(this.name, city, state);
+}
+
+// normal.call(obj, "non1", "non3");
+
+Function.prototype.newCall = function (context, ...args) {
+  context.fn = this;
+  context.fn(...args);
+};
+
+normal.newCall(obj, "non1", "non4");
