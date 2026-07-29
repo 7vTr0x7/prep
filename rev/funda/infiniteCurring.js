c0680c9450dfdc4117eda6aa14bc6879ea086e78
7vTr0x7 <7vtrox7@gmail.com>
2026-07-29 11:49:34 +0530

    infiniteCurring

---
+++ rev/funda/infiniteCurring.js
@@ -0,0 +1,14 @@
+const add = (a) => {
+  const inner = (b) => {
+    return add(a + b);
+  };
+
+  inner.valueOf = () => a;
+
+  inner.toString = () => String(a);
+
+  return inner;
+};
+
+console.log(add(1)(2)(3)(4) + 0); // 10
+console.log(add(5)(10)(15) + 0); // 30
