8ce3d854a7533c85132ff634a89d4276bbd389e2
7vTr0x7 <7vtrox7@gmail.com>
2026-08-19 14:52:36 +0530

    countOccurrences

--- rev/dsa3/string/countOccurrences.js
+++ rev/dsa3/string/countOccurrences.js
@@ -1,2 +1,15 @@
+const countOccurrences = (s) => {
+  let map = {};
 
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (map[char] !== undefined) {
+      map[char] += 1;
+    } else {
+      map[char] = 1;
+    }
+  }
+  return map;
+};
 
+console.log(countOccurrences("ssstringting"));
