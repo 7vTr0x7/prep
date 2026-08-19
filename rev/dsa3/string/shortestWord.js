86d595d4d83ad407d9d066ea336dd7253abe60de
7vTr0x7 <7vtrox7@gmail.com>
2026-08-19 14:03:24 +0530

    shortestWord

---
+++ rev/dsa3/string/shortestWord.js
@@ -0,0 +1,15 @@
+const shortestWord = (sen) => {
+  let words = sen.split(" ");
+  let shortest = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length < shortest.length) {
+      shortest = words[i];
+    }
+  }
+
+  return shortest;
+};
+
+console.log(shortestWord("I love JavaScript")); // I
+console.log(shortestWord("Find the shortest word")); // the
