75f6a32c6a4fbbf9b6bb8cbd8947fddfd1ad1feb
7vTr0x7 <7vtrox7@gmail.com>
2026-08-19 14:13:30 +0530

    reverseString

---
+++ rev/dsa3/string/reverseString.js
@@ -0,0 +1,11 @@
+const reverseString = (s) => {
+  let str = s.split("");
+
+  for (let i = 0; i < Math.floor(str.length / 2); i++) {
+    [str[i], str[str.length - 1 - i]] = [str[str.length - 1 - i], str[i]];
+  }
+
+  return str.join("");
+};
+
+console.log(reverseString("string"));
