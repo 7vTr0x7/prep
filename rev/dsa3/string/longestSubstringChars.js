59abab927e18b14eb4645d43d23cb53748660f22
7vTr0x7 <7vtrox7@gmail.com>
2026-08-19 15:10:10 +0530

    longestSubstringChars

---
+++ rev/dsa3/string/longestSubstringChars.js
@@ -0,0 +1,28 @@
+const longestSubstringChars = (str) => {
+  let map = {};
+  let maxLen = 0;
+  let start = 0;
+  let maxStart = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (map[char] !== undefined && map[char] >= start) {
+      start = map[char] + 1;
+    }
+
+    map[char] = i;
+
+    let length = i - start + 1;
+
+    if (length > maxLen) {
+      maxLen = length;
+      maxStart = start;
+    }
+  }
+
+  return str.substring(maxStart, maxStart + maxLen);
+};
+
+console.log(longestSubstringChars("abcabcbb")); // abc
+console.log(longestSubstringChars("bbbbb")); // b
