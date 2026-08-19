6a74a1892f28498f19420ff3aaaa9b4f3fb945f6
7vTr0x7 <7vtrox7@gmail.com>
2026-08-19 15:20:55 +0530

    firstRepeatingChar

--- rev/dsa3/string/firstRepeatingChar.js
+++ rev/dsa3/string/firstRepeatingChar.js
@@ -1,2 +1,17 @@
+const firstRepeatingChar = (s) => {
+  const map = {};
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (map[char] === undefined) {
+      map[char] = i;
+    } else {
+      return char;
+    }
+  }
+
+  return null;
+};
+
 console.log(firstRepeatingChar("abcdbea"));
 console.log(firstRepeatingChar("abcdef")); // null
