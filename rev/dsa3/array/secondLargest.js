55af44dcb806c8019981ca4fe22f035100e5892b
7vTr0x7 <7vtrox7@gmail.com>
2026-08-19 16:54:49 +0530

    secondLargest

--- rev/dsa3/array/secondLargest.js
+++ rev/dsa3/array/secondLargest.js
@@ -1,6 +1,18 @@
+const secondLargest = (arr) => {
+  if (!arr.length) return null;
+  let l = arr[0];
+  let s = arr[0];
 
-
-
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > l) {
+      s = l;
+      l = arr[i];
+    } else if (arr[i] > s && l !== arr[i]) {
+      s = arr[i];
+    }
+  }
+  return s;
+};
 
 console.log(secondLargest([10, 5, 8, 20, 15]));
 console.log(secondLargest([10, 10]));
