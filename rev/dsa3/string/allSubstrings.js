e436706806812efd59014dadb199b484109dc87e
7vTr0x7 <7vtrox7@gmail.com>
2026-08-19 15:41:53 +0530

    allSubstrings

--- rev/dsa3/string/allSubstrings.js
+++ rev/dsa3/string/allSubstrings.js
@@ -1,2 +1,19 @@
+const allSubstrings = (s) => {
+  if (!s.length) return null;
 
+  let result = [];
+  for (let i = 0; i < s.length; i++) {
+    let current = "";
 
+    for (let j = i; j < s.length; j++) {
+      current += s[j];
+      result.push(current);
+    }
+  }
+
+  return result;
+};
+
+console.log(allSubstrings("abc"));
+console.log(allSubstrings("abcabcbb")); // abc
+console.log(allSubstrings("bbbbb")); // b
