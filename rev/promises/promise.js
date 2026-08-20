e165d3b9d7bfae4a308f28b241b1538e95a0f60f
7vTr0x7 <7vtrox7@gmail.com>
2026-08-20 20:19:07 +0530

    promise

--- rev/promises/promise.js
+++ rev/promises/promise.js
@@ -1,15 +1,34 @@
 function PromisePollyFill(executor) {
-    
+  let onRessolve, onReject;
+
+  const resolve = (value) => {
+    onRessolve(value);
+  };
+  const reject = (value) => {
+    onReject(value);
+  };
+
+  this.then = (callback) => {
+    onRessolve = callback;
+    return this;
+  };
+
+  this.catch = (callback) => {
+    onReject = callback;
+    return this;
+  };
+
+  executor(resolve, reject);
 }
 
-// const ex = new PromisePollyFill((res, rej) => {
-//   setTimeout(() => {
-//     res("res");
-//   }, 1000);
-// });
+const ex = new PromisePollyFill((res, rej) => {
+  setTimeout(() => {
+    res("res");
+  }, 1000);
+});
 
-// ex.then((d) => {
-//   console.log(d);
-// }).catch((e) => {
-//   console.log(e);
-// });
+ex.then((d) => {
+  console.log(d);
+}).catch((e) => {
+  console.log(e);
+});
