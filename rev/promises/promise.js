ce71bae41b435b9998bde0aa8e5282d863ce2568
7vTr0x7 <7vtrox7@gmail.com>
2026-08-20 18:38:41 +0530

    promise

---
+++ rev/promises/promise.js
@@ -0,0 +1,15 @@
+function PromisePollyFill(executor) {
+    
+}
+
+// const ex = new PromisePollyFill((res, rej) => {
+//   setTimeout(() => {
+//     res("res");
+//   }, 1000);
+// });
+
+// ex.then((d) => {
+//   console.log(d);
+// }).catch((e) => {
+//   console.log(e);
+// });
