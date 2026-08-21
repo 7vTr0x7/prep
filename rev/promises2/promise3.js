83f5f637608a8200d007ec032ae41b0c2c2c868e
7vTr0x7 <7vtrox7@gmail.com>
2026-08-21 15:11:50 +0530

    promise

--- rev/promises2/promise3.js
+++ rev/promises2/promise3.js
@@ -2,17 +2,79 @@ const PromisePolyfill = function (executor) {
   let state = "pending";
   let value;
 
+  const onFulfilledCallbacks = [];
+  const onRejectedCallbacks = [];
+
   const resolve = (val) => {
     if (state !== "pending") return;
 
+    if (val instanceof PromisePolyfill) {
+      return val.then(resolve, reject);
+    }
+
     state = "fulfilled";
     value = val;
+
+    onFulfilledCallbacks.forEach((cb) => cb(value));
   };
-  const reject = (val) => {};
+  const reject = (reason) => {
+    if (state !== "pending") return;
+
+    state = "rejected";
+    value = reason;
+
+    onRejectedCallbacks.forEach((cb) => cb(value));
+  };
+
+  this.then = function (onFulfilled, onRejected) {
+    return new PromisePolyfill((resolveNext, rejectNext) => {
+      const handleFulfilled = () => {
+        try {
+          if (typeof onFulfilled !== "function") {
+            rejectNext(value);
+            return;
+          }
+
+          const result = onFulfilled(value);
+          resolveNext(result);
+        } catch (error) {
+          rejectNext(error);
+        }
+      };
+      const handleRejected = () => {
+        try {
+          if (typeof onRejected !== "function") {
+            onRejected(value);
+            return;
+          }
+
+          const result = onRejected(value);
+          rejectNext(result);
+        } catch (error) {
+          rejectNext(error);
+        }
+      };
+
+      if (state === "fulfilled") {
+        setTimeout(handleFulfilled, 0);
+      } else if (state === "rejected") {
+        setTimeout(handleRejected, 0);
+      } else {
+        onFulfilledCallbacks.push(handleFulfilled);
+        onRejectedCallbacks.push(handleRejected);
+      }
+    });
+  };
+
+  this.catch = function (onRejected) {
+    return this.then(null, onRejected);
+  };
 
   try {
     executor(resolve, reject);
-  } catch (error) {}
+  } catch (error) {
+    reject(error);
+  }
 };
 
 const promise = new PromisePolyfill((resolve, reject) => {
