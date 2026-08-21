28c40f2c3e37d695c36e3639244fbdc5395fadfd
7vTr0x7 <7vtrox7@gmail.com>
2026-08-21 17:14:49 +0530

    apply

--- rev/funda1/apply.js
+++ rev/funda1/apply.js
@@ -1,1 +1,15 @@
+const obj = {
+  name: "non",
+};
 
+function fun(v) {
+  console.log(this.name, v);
+}
+
+Function.prototype.newApply = function (context, args) {
+  if (!Array.isArray(args)) return;
+  context.fn = this;
+  context.fn(...args);
+};
+
+fun.newApply(obj, ["non1"]);
