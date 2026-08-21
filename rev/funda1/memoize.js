6dd62f9ccc1c3edf7815c8557556d2b2244a5981
7vTr0x7 <7vtrox7@gmail.com>
2026-08-21 17:57:11 +0530

    memoize

--- rev/funda1/memoize.js
+++ rev/funda1/memoize.js
@@ -1,13 +1,26 @@
+const memoize = (fn) => {
+  let cache = {};
+
+  return function (...args) {
+    let key = JSON.stringify(args);
+    if (cache[key] === undefined) {
+      cache[key] = fn(...args);
+    }
+    return cache[key];
+  };
+};
+
 const heavyFunc = (n1, n2) => {
   for (let i = 0; i < 1000000000; i++) {}
 
   return n1 * n2;
 };
 
+const memoized = memoize(heavyFunc);
 console.time("f");
-console.log(heavyFunc(400, 500));
+console.log(memoized(400, 500));
 console.timeEnd("f");
 
 console.time("s");
-console.log(heavyFunc(400, 500));
+console.log(memoized(400, 500));
 console.timeEnd("s");
