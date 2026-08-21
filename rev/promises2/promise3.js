1734a7ac751c76fef7a662de8e901690fa4c2d33
7vTr0x7 <7vtrox7@gmail.com>
2026-08-21 14:26:01 +0530

    promise

--- rev/promises2/promise3.js
+++ rev/promises2/promise3.js
@@ -1,3 +1,20 @@
+const PromisePolyfill = function (executor) {
+  let state = "pending";
+  let value;
+
+  const resolve = (val) => {
+    if (state !== "pending") return;
+
+    state = "fulfilled";
+    value = val;
+  };
+  const reject = (val) => {};
+
+  try {
+    executor(resolve, reject);
+  } catch (error) {}
+};
+
 const promise = new PromisePolyfill((resolve, reject) => {
   setTimeout(() => {
     resolve(10);
